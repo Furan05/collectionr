916c068c27c5f9289551a3c9640106981aabd388
Alexis Ferrere <alexferrere@outlook.fr>
2024-12-13 12:57:58 +0100

    done

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -17,9 +17,13 @@ class FavoritesController < ApplicationController
     @favorite = current_user.favorites.find_by(card_id: params[:card_id])
     @favorite.destroy
 
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_back(fallback_location: root_path) }
+    if @favorite.save
+      respond_to do |format|
+        format.turbo_stream
+        format.html { redirect_back(fallback_location: root_path) }
+      end
+    else
+      redirect_back(fallback_location: root_path, alert: 'Cannot add more than 5 favorites')
     end
   end
 end
