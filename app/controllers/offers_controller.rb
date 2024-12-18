303ad1cc19655165eddc91ee218118b018bcfdd1
Enzo Lanne <enzolanne@gmail.com>
2024-12-18 10:27:17 +0100

    publication d'offres

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -12,13 +12,24 @@ class OffersController < ApplicationController
   end
 
   def create
-    @offer = Offer.new(offer_params)
+    @offer = current_user.offers.new(offer_params.except(:image_url))
+
+    if params[:offer][:image_url].present?
+      image_path = params[:offer][:image_url].tempfile.path
+      image_url = upload_image(image_path)
+      @offer.image_url = image_url if image_url
+    end
 
     if @offer.save
-      redirect_to @offer
+      redirect_to @offer, notice: 'Offer was successfully created.'
     else
-      render :new
+      # Ensure you're always redirecting or rendering
+      render :new, status: :unprocessable_entity
     end
+  rescue => e
+    # Add error logging
+    Rails.logger.error "Offer creation failed: #{e.message}"
+    render :new, status: :unprocessable_entity
   end
 
   def edit
@@ -28,7 +39,7 @@ class OffersController < ApplicationController
   def update
     @offer = Offer.find(params[:id])
     @offer.update(offer_params)
- 
+
     redirect_to @offer
   end
 
@@ -42,7 +53,31 @@ class OffersController < ApplicationController
   private
 
   def offer_params
-    params.require(:offer).permit(:title, :description, :price, :location)
+    params.require(:offer).permit(:title, :price, :condition, :bio, :langue, :graduation, :image_url, :card_id)
   end
 
+  def upload_image(image_path)
+    begin
+      base64_image = Base64.strict_encode64(File.open(image_path, 'rb').read)
+      full_base64 = "data:image/png;base64,#{base64_image}"
+
+      response = HTTParty.post('https://lannetech.com/api/upload.php',
+          body: { image: full_base64 }.to_json,
+          headers: {
+              'Content-Type' => 'application/json',
+              'Accept' => 'application/json'
+          }
+      )
+
+      if response.success? && response.parsed_response['success']
+        response.parsed_response['url']
+      else
+        Rails.logger.error "Image upload failed: #{response.body}"
+        nil
+      end
+    rescue => e
+      Rails.logger.error "Image upload error: #{e.message}"
+      nil
+    end
+  end
 end
